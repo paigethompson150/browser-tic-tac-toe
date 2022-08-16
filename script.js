bcb164e59892fcd23670231e67f2d937c278ec5a
404paigenotfound <92485057+paigethompson150@users.noreply.github.com>
2022-08-15 23:50:59 -0400

    computer can make random decisions

--- script.js
+++ script.js
@@ -9,6 +9,15 @@ const WIN_COMBOS = [
   [0,4,8],
   [2,4,6]
 ]
+
+const OG_BOARD_POSITIONS = [
+  0, 1, 2,
+  3, 4, 5,
+  6, 7, 8,
+];
+
+let endGame = false;
+let turnTracker = false;
 //module used for keeping track of and updating board positions
 const gameBoard = (() => {
   boardPositions = [
@@ -17,8 +26,30 @@ const gameBoard = (() => {
     6, 7, 8,
   ];
   const addMove = (player, location, label) => {
-    boardPositions[location] = label;
-    updateBoard(player, boardPositions, location, label);
+    //if no winner has been declared yet
+    if (endGame === false){
+      //if it's the computer - check if the random space is filled before switching turns
+      if (player.name == 'demagotron'){
+        if (boardPositions[location] != OG_BOARD_POSITIONS[location]){
+          gameBoard.addMove(computer, Math.floor(Math.random()*(9)), label);
+        }
+        else {
+          turnTracker = false;
+          boardPositions[location] = label;
+          updateBoard(player, boardPositions, location, label);
+        }
+      }
+      if (player.name != 'demagotron'){
+        if((boardPositions[location] == OG_BOARD_POSITIONS[location])){
+        turnTracker = true;
+        boardPositions[location] = label;
+        updateBoard(player, boardPositions, location, label);
+        }
+        else {
+        console.log('not a legal move');
+        }
+      }
+    }
     //player makes move here
     //update boardPositions array
   }
@@ -26,8 +57,23 @@ const gameBoard = (() => {
     gridItem[location].innerHTML = label;
     if (checkForWinner(player, boardPositions) == true){
       console.log(player.name + 'wins');
+      endGame = true;
+    }
+    else {
+      if (checkForTie(player, boardPositions) == true){
+        console.log('its a tie!');
+        endGame = true;
+      }
     }
   }
+  const checkForTie = (player, boardPositions) => {
+    for(let n = 0; n < boardPositions.length; n++){
+      if (boardPositions[n] == OG_BOARD_POSITIONS[n]){
+        return false;
+      }
+    }
+  }
+
   const checkForWinner = (player, boardPositions) => {
     for(let k = 0; k < WIN_COMBOS.length; k++){
       let item = WIN_COMBOS[k];
@@ -38,9 +84,7 @@ const gameBoard = (() => {
     }
   }
   return {
-    addMove,
-    updateBoard,
-    checkForWinner
+    addMove
   };
 
 })();
@@ -57,8 +101,20 @@ function showBoard(){
   gameBoardUI.classList.remove('hidden');
   gameBoardUI.classList.add('fade-in');
   gameBoardUI.classList.add('visible');
-
 }
+
+let gridItem = document.querySelectorAll('.gridItem');
+  for(let i =0; i < gridItem.length; i++){
+    gridItem[i].addEventListener('click', function(){
+      //pass in the user info, as well as id of grid item pressed on
+      gameBoard.addMove(user, gridItem[i].id, user.label);
+      if (turnTracker === true){
+        gameBoard.addMove(computer, Math.floor(Math.random()*(9)), computer.label);
+      }
+    });
+  }
+
+
 
 const introGraphic = document.querySelector('.intro');
 const gameBoardUI = document.getElementById('gameBoard');
@@ -70,10 +126,3 @@ const user = player('paige', 'x');
 const computer = player('demagotron', 'o');
 
 //add an event listener onto each button
-let gridItem = document.querySelectorAll('.gridItem');
-for(let i =0; i < gridItem.length; i++){
-  gridItem[i].addEventListener('click', function(){
-    //pass in the user info, as well as id of grid item pressed on
-    gameBoard.addMove(user, gridItem[i].id, user.label);
-  });
-}
