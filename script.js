81a3f9d38a6e63ea34875f7cb2894aa9644bdf02
404paigenotfound <92485057+paigethompson150@users.noreply.github.com>
2022-08-15 22:10:28 -0400

    added event listeners on buttons

--- script.js
+++ script.js
@@ -5,16 +5,66 @@
 
 //set player as a factory
 
+//winning combinations
+const WIN_COMBOS = [
+  [0,1,2],
+  [3,4,5],
+  [6,7,8],
+  [0,3,6],
+  [2,5,8],
+  [0,4,8],
+  [2,4,6]
+]
+//module used for keeping track of and updating board positions
+const gameBoard = (() => {
+  boardPositions = [
+    'n', 'n', 'n',
+    'n', 'n', 'n',
+    'n', 'n', 'n',
+  ];
+  const addMove = (player, location) => {
+    console.log(location);
+    //player makes move here
+    //update boardPositions array
+  }
+  return {
+    addMove
+  };
+
+})();
+
+//name can either be user/computer, label is x or o
+const player = (name, label) => {
+  return { name, label }
+}
+
 
 function showBoard(){
-  console.log('test');
   introGraphic.classList.add('slide_out');
   introGraphic.classList.add('hidden');
-  gameBoard.classList.remove('hidden');
-  gameBoard.classList.add('fade-in');
-  gameBoard.classList.add('visible');
+  gameBoardUI.classList.remove('hidden');
+  gameBoardUI.classList.add('fade-in');
+  gameBoardUI.classList.add('visible');
 
 }
+
 const introGraphic = document.querySelector('.intro');
-const gameBoard = document.getElementById('gameBoard');
+const gameBoardUI = document.getElementById('gameBoard');
 document.addEventListener('click', showBoard);
+
+//create new user player
+//we could adjust the name here to be == to input from DOM
+const user = new player('paige', 'x');
+
+//add an event listener onto each button
+let gridItem = document.querySelectorAll('.gridItem');
+
+function testFunc(){
+  console.log('testing');
+}
+
+for(let i =0; i < gridItem.length; i++){
+  gridItem[i].addEventListener('click', function(){
+    gameBoard.addMove(user,);
+  });
+}
